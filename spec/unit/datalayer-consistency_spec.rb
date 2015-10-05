8830b7a6f430ff757aa84940ba5c38966702e722
Thomas Schank <DrTom@schank.ch>
2015-10-05 10:30:47 +0200

    Add datalayer commit consistency test; improve tests config

---
+++ spec/unit/datalayer-consistency_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'git'
+
+describe 'The datalayer of the Webapp and the API' do
+  def commit_id path
+    sha = Git.open(path).object('HEAD').sha
+    fail "'#{sha}' is not a valid object id" unless sha =~ /^[0-9a-fA-F]{40}$/
+    sha
+  end
+
+  it 'Reference exactly the same commit' do
+    expect(commit_id('../webapp/engines/datalayer')).to \
+      be == commit_id('../api/datalayer')
+  end
+end
