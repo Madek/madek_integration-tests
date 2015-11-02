172994012c7a4ac2e91ddba7476756f19d09829e
Thomas Schank <DrTom@schank.ch>
2015-11-02 13:51:17 +0100

    Finish API partial download test

--- spec/features/media-file_up-and-download_spec.rb
+++ spec/features/media-file_up-and-download_spec.rb
@@ -1,4 +1,7 @@
+require 'addressable/uri'
 require 'spec_helper'
+
+GRUMPY_CAT_PATH= File.absolute_path('../api/datalayer/spec/data/images/grumpy_cat.jpg')
 
 feature 'MediaEntry' do
   scenario 'Up- and download' do
@@ -29,8 +32,7 @@ feature 'MediaEntry' do
 
     # select file and submit
     within('.app-body') do
-      attach_file('media_entry_media_file',
-                  File.absolute_path('../api/datalayer/spec/data/images/grumpy_cat.jpg'))
+      attach_file('media_entry_media_file', GRUMPY_CAT_PATH)
       find("*[type='submit']").click
     end
 
@@ -42,5 +44,29 @@ feature 'MediaEntry' do
     click_on 'Publish!'
 
     accept_prompt
+
+    click_on 'API'
+    api_click_on_relation_method 'media-entries', 'GET'
+    click_on 'Continue'
+    api_click_on_relation_method '1', 'GET'
+    api_click_on_relation_method 'media-file', 'GET'
+    api_click_on_relation_method 'data-stream', 'GET'
+    expect(page).to have_content "200 OK"
+
+    # Download via API
+    data_stream_path = Addressable::URI.parse(current_url).fragment
+    url = Capybara.app_host + data_stream_path
+    buffer = `curl -u adam:password #{url}`
+    digest_original= Digest::SHA1.file(GRUMPY_CAT_PATH).hexdigest
+    digest_download= Digest::SHA1.hexdigest(buffer)
+    expect(digest_download).to be== digest_original
+
+
+    # PARTIAL DL via API
+    partial_digest= Digest::SHA1.new
+    partial_digest.update `curl -r 0-999 -u adam:password #{url}`
+    partial_digest.update `curl -r 1000- -u adam:password #{url}`
+    expect(partial_digest.hexdigest).to be== digest_original
+
   end
 end
