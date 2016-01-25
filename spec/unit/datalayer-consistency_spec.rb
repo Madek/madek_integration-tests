3aed73bf335fcc7d115c086038f3af543044495e
Michał Podlecki <m.podlecki@op.pl>
2016-01-25 10:12:39 +0100

    integration test checking that Webapp, Admin Webapp & API use the same version of the datalayer

--- spec/unit/datalayer-consistency_spec.rb
+++ spec/unit/datalayer-consistency_spec.rb
@@ -1,15 +1,31 @@
 require 'spec_helper'
 require 'git'
 
-describe 'The datalayer of the Webapp and the API' do
+describe 'The datalayer of the Webapp, Admin Webapp and the API' do
   def commit_id(path)
     sha = Git.open(path).object('HEAD').sha
     fail "'#{sha}' is not a valid object id" unless sha =~ /^[0-9a-fA-F]{40}$/
     sha
   end
 
-  it 'Reference exactly the same commit' do
-    expect(commit_id('../webapp/engines/datalayer')).to \
-      be == commit_id('../api/datalayer')
+  context 'Webapp & API' do
+    it 'References exactly the same commit' do
+      expect(commit_id('../webapp/engines/datalayer')).to \
+        be == commit_id('../api/datalayer')
+    end
+  end
+
+  context 'Admin Webapp & API' do
+    it 'References exactly the same commit' do
+      expect(commit_id('../admin-webapp/engines/datalayer')).to \
+        be == commit_id('../api/datalayer')
+    end
+  end
+
+  context 'Webapp & Admin Webapp' do
+    it 'References exactly the same commit' do
+      expect(commit_id('../webapp/engines/datalayer')).to \
+        be == commit_id('../admin-webapp/engines/datalayer')
+    end
   end
 end
