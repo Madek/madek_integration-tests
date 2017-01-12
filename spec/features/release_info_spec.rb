f658a4d4a9212fea94a8b64c0bf8bbd34ac588cc
Max Albrecht <max.albrecht@zhdk.ch>
2017-01-12 13:46:50 +0100

    spec: release info

---
+++ spec/features/release_info_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'webapp: release info', type: :feature do
+  before :example do
+    # check needed config file, see spec setup for fixtures
+    raise 'missing config!' unless File.exist?('../config/releases.yml')
+    raise 'missing config!' unless File.exist?('../config/deploy-info.yml')
+  end
+
+  it 'shows version info in footer' do
+    visit '/'
+
+    info = first('.ui-footer-copy a')
+    expect(info.text).to eq 'Madek v1.0.0-1 Second'
+    expect(info[:href]).to eq "#{Capybara.app_host}/release"
+  end
+
+  it 'shows deploy and release info on releases page' do
+    visit '/release'
+
+    # NOTE: this expects the client to be on UTC timezone (like CI server)
+    expect(find('.ui-container.bright').text)
+      .to eq <<-TEXT.strip_heredoc.tr("\n", ' ').gsub('  ', ' ').strip
+
+        Letztes Deployment: Freitag, 5. August 2016 12:35
+        Madek v1.0.0-1 "Second"
+        second release (pre)
+
+        Vorherige Versionen
+        Madek 1.0.0 "First"
+        first release
+
+      TEXT
+  end
+
+end
