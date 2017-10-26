7eec96ba9971ad4b97a0cb829cf0f65231e1ebf1
Michał Podlecki <m.podlecki@op.pl>
2017-10-26 08:57:01 +0200

    spec: linking admin/people -> webapp/people

---
+++ spec/features/linking-from-admin-people-to-webapp-people_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'Linking from admin people to webapp people', type: :feature do
+  it 'goes to a correct person page' do
+    visit '/'
+    login_as_database_user
+    visit '/admin/people'
+    fill_in 'filter[search_term]', with: 'Adam Admin'
+    click_button 'Apply'
+
+    ui_window = window_opened_by do
+      within 'table tbody tr:first-of-type' do
+        click_link 'Show in UI'
+      end
+    end
+
+    within_window ui_window do
+      expect(page).to have_content 'Adam Admin'
+      expect(current_path).to start_with '/people/'
+    end
+  end
+end
